707d0cdc02f1b930fc2b0aec6998217347e66fda
Krystof272 <krystof272@gmail.com>
2026-01-19 21:00:50 +0100

    added console ini and start

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Tohle bude fajn schovat do nějaké třídy, ve které budeme pracovat se všemi daty a hráčem
-        GameData datos = GameData.loadGameDataFromResources("/gameData.json");
-
-        System.out.println(datos);
+        Console console = new Console();
+        console.start();
     }
 }
