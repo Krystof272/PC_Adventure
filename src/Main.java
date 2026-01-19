6cd540825c6b3138046c4cd94ecfe26d86aba19b
Krystof272 <krystof272@gmail.com>
2026-01-19 20:24:12 +0100

    added and tested loading

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //Tohle bude fajn schovat do nějaké třídy, ve které budeme pracovat se všemi daty a hráčem
+        GameData datos = GameData.loadGameDataFromResources("/gameData.json");
 
+        System.out.println(datos);
     }
 }
