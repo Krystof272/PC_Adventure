b2b59560edc2a05fd4dae3e441ef72afd33c96c9
Krystof272 <krystof272@gmail.com>
2026-01-21 17:12:18 +0100

    added function for spliting intputComand

--- src/Console.java
+++ src/Console.java
@@ -1,33 +1,32 @@
-import Commands.Command;
-import Commands.Jdi;
-
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Console {
-    private static HashMap<String, Command> comands;
+    private HashMap<String, Command> comands;
     private boolean isExit;
     private Scanner sc;
     private GameData datos = GameData.loadGameDataFromResources("/gameData.json");
+    private Player player;
 
     public Console() {
         this.comands = new HashMap<>();
         this.isExit = false;
         this.sc = new Scanner(System.in);
+        this.player = new Player("Player");
     }
 
     public void inicialization(){
         comands.put("jdi", new Jdi());
-        comands.put("help", new Help());
     }
 
     public void execute(){
         System.out.print(">> ");
-        String command = sc.next();
+        String inputCommand = sc.nextLine();
+        String[] commnads = inputCommand.split(" ");
 
-        if (comands.containsKey(command)){
-            System.out.println(comands.get(command).execute());
-            isExit = comands.get(command).isExit();
+        if (comands.containsKey(commnads[0])){
+            System.out.println(comands.get(commnads[0]).execute(commnads[1], player, datos));
+            isExit = comands.get(commnads[0]).isExit();
         } else {
             System.out.println("Command not recognized");
         }
@@ -35,15 +34,9 @@ public class Console {
 
     public void start(){
         inicialization();
-
         //TODO nacitani jmena hrace z konzole
-        Player player = new Player("Krystof");
         do{
             execute();
         } while (!isExit);
-    }
-
-    public static String getCommands(){
-        return comands.keySet().toString();
     }
 }
