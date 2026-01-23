04fbbadf582fc3fa8eba65338d9058f824002bfa
Krystof272 <krsytof272@gmail.com>
2026-01-23 11:11:40 +0100

    added complete logic

--- src/Use.java
+++ src/Use.java
@@ -1,8 +1,27 @@
+/**
+ * this class uses items from player inventory
+ */
 public class Use implements Command {
 
+    /**
+     * this method moves item from player inventory to location
+     *
+     * @param command requested item
+     * @param player current playing player
+     * @param datos object filled with data
+     * @return returns if the usage was successful or there were any problems
+     */
     @Override
     public String execute(String command, Player player, GameData datos) {
-        return "";
+        if(player.removeItem(command)){
+            if(datos.getLocation(player.getCurrentLocationId()).addItem(command)){
+                return "Predmet "+ command +" odebran z inventare.";
+            } else {
+                player.addItem(command);
+                return "Do teto lokace tento predmet dat nemuzes!";
+            }
+        }
+        return "Item nenalezen v inventari!";
     }
 
     @Override
