e535c11006a34475d92ec5b08caadeb078ba245d
Krystof272 <krsytof272@gmail.com>
2026-01-23 10:37:20 +0100

    removed item

--- src/GameData.java
+++ src/GameData.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
  *
  */
 public class GameData {
-    private ArrayList<Item> items;
     private ArrayList<NPC> npcs;
     private Bugisek bugisek;
     private ArrayList<Location> locations;
@@ -63,8 +62,7 @@ public class GameData {
     @Override
     public String toString() {
         return "GameData{" +
-                "items=" + items +
-                ", npcs=" + npcs +
+                "npcs=" + npcs +
                 ", bugisek=" + bugisek +
                 ", locations=" + locations +
                 '}';
