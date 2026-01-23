c25108bfd279822f6d249f6d968c921fec15637a
Krystof272 <krsytof272@gmail.com>
2026-01-23 09:43:09 +0100

    added logic function

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,8 +1,14 @@
+/**
+ * this class returns contents of the inventory
+ */
 public class Inventory implements Command {
 
     @Override
     public String execute(String command, Player player, GameData datos) {
-        return "";
+        if (command.equals("get")) {
+            return player.getInventory();
+        }
+        return "Pouzij: inventory + get!";
     }
 
     @Override
