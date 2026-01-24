6dc9634cbc350da9aaf27fdc05ee7517ca77375b
Krystof272 <krystof272@gmail.com>
2026-01-24 19:24:08 +0100

    changed return string

--- src/Help.java
+++ src/Help.java
@@ -2,7 +2,7 @@ public class Help implements Command {
     @Override
     public String execute(String command, Player player, GameData datos) {
         if (command.equals("hrac")) {
-            return "[jdi <sever, jih, vychod, zapad>; seber <predmet>; pouzij <predmet>; inventar <hrac>; mluv <postava>; odpoved <volba>]";
+            return "[jdi <sever, jih, vychod, zapad>; seber <predmet>; pouzij <predmet>; inventar <hrac>; mluv <postava>; odpoved <volba>; pomoc <hrac>]";
         }
         return "Pouzij: pomoc <hrac>!";
     }
