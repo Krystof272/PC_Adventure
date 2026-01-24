32ed039717cd5b71c70fdbcda9872bafc51f96d6
Krystof272 <krystof272@gmail.com>
2026-01-24 16:07:26 +0100

    added method logic

--- src/Answer.java
+++ src/Answer.java
@@ -1,8 +1,23 @@
+/**
+ * this class answers npcs questions
+ */
 public class Answer implements Command {
 
+    /**
+     * this method returns answer
+     *
+     * @param command player chosen answer
+     * @param player  current playing player
+     * @param datos   object filled with data
+     * @return returns the answer from the npc
+     */
     @Override
     public String execute(String command, Player player, GameData datos) {
-        return "";
+        NPC npc = datos.getNPC(player.getCurrentTalkingNPC());
+        if (npc != null) {
+            return npc.getAnswer(command);
+        }
+        return "Nejdrive musis zacit konverzaci s npc!";
     }
 
     @Override
