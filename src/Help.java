55f37d24736e8e62685e4606e4740c164c3149ef
Krystof272 <krystof272@gmail.com>
2026-01-24 13:07:49 +0100

    added method logic

--- src/Help.java
+++ src/Help.java
@@ -1,8 +1,10 @@
 public class Help implements Command {
-
     @Override
     public String execute(String command, Player player, GameData datos) {
-        return "";
+        if (command.equals("hrac")) {
+            return "[jdi <sever, jih, vychod, zapad>; seber <predmet>; pouzij <predmet>; inventar <hrac>; mluv <postava>; odpoved <volba>]";
+        }
+        return "Pouzij: pomoc <hrac>!";
     }
 
     @Override
