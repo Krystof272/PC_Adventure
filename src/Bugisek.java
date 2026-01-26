8159d67c50fa50f0e51a586342c125e6c41ceebc
Krystof272 <krsytof272@gmail.com>
2026-01-26 20:05:09 +0100

    added class logic

--- src/Bugisek.java
+++ src/Bugisek.java
@@ -1,19 +1,58 @@
-public class Bugisek {
-    public String location;
-    //TODO maybe smazat
+import java.util.Arrays;
+import java.util.Random;
 
-    public String getLocation() {
-        return location;
+/**
+ * this class creates and moves bugisek, evaulates if player encountered him
+ */
+public class Bugisek {
+    private String[] location;
+
+    public Bugisek() {
+        this.location = new String[2];
+        location[0] = "hdd";
     }
 
-    public boolean move(){
+    /**
+     * this method evaulates if bugisek and player met
+     *
+     * @param currentLocation of player
+     * @param newLocation of player
+     * @return returns true if they met
+     */
+    public boolean meetWithPlayer(String currentLocation, String newLocation) {
+        if (currentLocation.equals(location[1]) && newLocation.equals(location[0])) {
+            return true;
+        } else if (currentLocation.equals(location[0]) && newLocation.equals(location[1])) {
+            return true;
+        }
         return false;
+    }
+
+    /**
+     * this method moves bugisek, it assignees second location to first and first location to seconds locations neighbour
+     *
+     * @param datos data container
+     * @return returns true if the movement happened
+     */
+    public boolean move(GameData datos) {
+        Random rnd = new Random();
+        String[] directions = new String[4];
+        directions[0] = "sever";
+        directions[1] = "jih";
+        directions[2] = "vychod";
+        directions[3] = "zapad";
+
+        location[1] = location[0];
+        do {
+            location[0] = datos.getLocation(location[1]).getNeighbour(directions[rnd.nextInt(directions.length)]);
+        } while (location[0] == null || location[0].isEmpty());
+        return true;
     }
 
     @Override
     public String toString() {
         return "Bugisek{" +
-                "location='" + location + '\'' +
+                "location=" + Arrays.toString(location) +
                 '}';
     }
 }
