1693140181b7a13f6edc925bf3ed3a19827ab70f
Krystof272 <krsytof272@gmail.com>
2026-02-02 13:30:52 +0100

    remove isExit and condition

--- src/Help.java
+++ src/Help.java
@@ -1,14 +1,6 @@
 public class Help implements Command {
     @Override
     public String execute(String command, Player player, GameData datos) {
-        if (command.equals("hrac")) {
-            return "[jdi <sever, jih, vychod, zapad>; seber <predmet>; pouzij <predmet>; inventar <hrac>; mluv <postava>; odpoved <volba>; mapa <neco>; pomoc <hrac>]";
-        }
-        return "Pouzij: pomoc <hrac>!";
-    }
-
-    @Override
-    public boolean isExit() {
-        return false;
+        return "[jdi <sever, jih, vychod, zapad>; seber <predmet>; pouzij <predmet>; inventar; mluv <postava>; odpoved <volba>; mapa; pomoc]";
     }
 }
