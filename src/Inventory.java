0452a1e04be574697f65983e96e5f3b8bdbb10ff
Krystof272 <krsytof272@gmail.com>
2026-02-02 13:30:43 +0100

    remove isExit and condition

--- src/Inventory.java
+++ src/Inventory.java
@@ -5,14 +5,6 @@ public class Inventory implements Command {
 
     @Override
     public String execute(String command, Player player, GameData datos) {
-        if (command.equals("hrac")) {
-            return player.getInventory();
-        }
-        return "Pouzij: inventory <hrac>!";
-    }
-
-    @Override
-    public boolean isExit() {
-        return false;
+        return player.getInventory();
     }
 }
